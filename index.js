a7f08f73b946062e12f0681ca34e03667b76f42a
unknown <rajebul.alam17@gmail.com>
2022-05-27 21:22:20 +0600

    Eleted Product by User from DB

--- index.js
+++ index.js
@@ -51,6 +51,14 @@ async function run() {
             res.send(result);
         });
 
+        // Delete Product from Order
+        app.delete('/items/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderedCollection.deleteOne(query);
+            res.send(result);
+        });
+
     }
     finally {
 
