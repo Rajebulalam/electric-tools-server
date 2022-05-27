a4bd10eaf88dc28788b8c0e3ab219559083c1a9f
unknown <rajebul.alam17@gmail.com>
2022-05-27 11:49:58 +0600

    Load Product from DB

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -18,7 +19,21 @@ async function run() {
     try {
         await client.connect();
         const productionCollection = client.db("production").collection("product");
-        console.log('Data Base connected');
+
+        // Get All Product from Db
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const result = await productionCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        // Get Product by Id
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productionCollection.findOne(query);
+            res.send(result);
+        });
 
     }
     finally {
